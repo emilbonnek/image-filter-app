60f73e88c8c051b929e4a8eb0f8fc47f5a57974a
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2022-10-14 21:47:01 +0200

    added input tag for uploads

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { createFileUploader } from "@solid-primitives/upload";
+import { Component, createSignal } from "solid-js";
 
 const App: Component = () => {
+  const { files, selectFiles } = createFileUploader({ accept: "image/*" });
+
+  function handleUpload(event: Event) {
+    console.log(event);
+  }
+
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div>
+      <input type="file" accept="image/*" onChange={handleUpload} />
     </div>
   );
 };
